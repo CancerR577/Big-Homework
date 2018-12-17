d7177a9753a5924d5724c532a65aed50c8b5a936
Tiffany He <1700012189@pku.edu.cn>
2018-12-17 08:12:20 +0800

    Revert "在旧版本Enterbox的基础上增加了图片和点阵"
    This reverts commit 973df3a2561ffc7acc292b4ef52c9850c9e486e7.

--- OurWidgets/EnterBox/EnterBox.h
+++ OurWidgets/EnterBox/EnterBox.h
@@ -2,14 +2,6 @@
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Button.H>
 #include <FL/Fl_Box.H>
-#include <FL/Fl_PNG_Image.H>
-#include <FL/Fl_Shared_Image.H>
-#include <vector>
-
-void set_level() {}; // unaccomplished
-enum Hard_level{easy, normal, hard};
-void creat_dot(Hard_level l);
-void creat_box(Hard_level l);
 
 class EnterBox : public Fl_Box
 {
@@ -20,7 +12,7 @@ public:
         switch (e)
         {
             case  FL_ENTER://鼠标移上来
-                 color(FL_DARK_MAGENTA);
+                 color(FL_GREEN);
                  redraw();
                  return 1;
             case  FL_LEAVE://鼠标移开
@@ -31,9 +23,7 @@ public:
                 switch (Fl::event_button())
                 {
                 case FL_LEFT_MOUSE:
-                    fl_register_images();
-                    m_pSharedImage = Fl_Shared_Image::get("work.PNG");
-                    image(m_pSharedImage->copy(w(),h()+15));
+                    color(FL_RED);
                     enable = false;//让这个Box不能再响应
                     redraw();
                     return 1;
@@ -45,57 +35,5 @@ public:
     EnterBox(int x, int y, int w, int h, const char *l, bool ee ) : Fl_Box(x,y,w,h,l), enable{ee} {}
 
 private:
-    Fl_Shared_Image* m_pSharedImage;//图片
     bool enable;
 };
-
-class DotBox : public Fl_Box
-{
-public:
-    DotBox(int X = 25,int Y = 50,int W = 10,int H = 10):Fl_Box(X,Y,W,H)
-    {
-        fl_register_images();
-        m_pSharedImage = Fl_Shared_Image::get("work.PNG");
-        image(m_pSharedImage->copy(w(),h()));
-        redraw();
-    }
-private:
-    Fl_Shared_Image* m_pSharedImage;
-
-};
-
-void creat_dot(Hard_level l, DotBox* ar[][6])
-{
-    switch(l){
-    case easy:
-        for(int i=0; i<2; i++)
-        {
-            for(int j=0; j<2; j++)
-            {
-                ar[i][j] = new DotBox(25 + 70*i,50 + 70*j,10,10);
-            }
-
-        }
-    break;
-    case normal:
-        for(int i=0; i<4; i++)
-        {
-            for(int j=0; j<4; j++)
-            {
-                ar[i][j] = new DotBox(25 + 70*i,50 + 70*j,10,10);
-            }
-
-        }
-    break;
-    case hard:
-       for(int i=0; i<6; i++)
-        {
-            for(int j=0; j<6; j++)
-            {
-                ar[i][j] = new DotBox(25 + 70*i,50 + 70*j,10,10);
-            }
-
-        }
-    }
-}
-
