cd1648aa1a0519e8c7b220348e7736a1650729a9
CancerR577 <251603209@qq.com>
2018-12-23 17:17:42 +0800

    修改了Ver2.2的小bug，稍微精简了代码
    稍微精简了EnterBox和SquareUnit的代码
    把EnterBox的isable属性和SquareUnit的deg及player属性都藏起来了，初始化直接采用默认值

---
+++ OurGame/Ver2.3/EnterBox.h
@@ -0,0 +1,43 @@
+#ifndef _ENTERBOX_H_
+#define _ENTERBOX_H_
+
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Shared_Image.H>
+#include <FL/Fl_PNG_Image.H>
+#include <Fl/Enumerations.H>
+#include "SquareUnit.h"
+
+class EnterBox : public Fl_Box
+{
+public:
+    int handle(int e);
+    int getisable() { return isable; }
+    static void cb_activate(Fl_Widget*, void*);
+
+    EnterBox(int x, int y, int w, int h, SquareUnit *s)
+        : Fl_Box(x,y,w,h)
+        , isable{true}
+        , square{s}
+        , square2{nullptr}
+        {
+        }
+
+    EnterBox(int x, int y, int w, int h, SquareUnit *s, SquareUnit *s2)
+        : Fl_Box(x,y,w,h)
+        , isable{true}
+        , square{s}
+        , square2{s2}
+        {
+        }
+private:
+    bool isable;
+    Fl_Shared_Image *eBox_pic;//图片
+    SquareUnit *square;//用于激活相应的SquareUnit
+    SquareUnit *square2;//大部分边对应两个方块
+};
+
+#endif // _ENTERBOX_H_
+
