042caa67fe149e5c6d9b5de1d23d1f1de091f1d0
CancerR577 <251603209@qq.com>
2019-01-03 15:50:26 +0800

    调整了自动调整棋盘大小的代码

---
+++ OurGame/Ver5.21/DotBox.h
@@ -0,0 +1,36 @@
+#include <FL/Fl_Box.H>
+#ifndef DOTBOX_H_INCLUDED
+#define DOTBOX_H_INCLUDED
+
+class DotBox : public Fl_Box
+{
+public:
+    DotBox(int X = 50,int Y = 100,int W = 15,int H = 15, int choice = 0):Fl_Box(X,Y,W,H)
+    {
+        fl_register_images();
+        switch(choice){
+        case 0:
+            m_pSharedImage = Fl_Shared_Image::get("work4.PNG");
+            break;
+        case 1:
+            m_pSharedImage = Fl_Shared_Image::get("Wizard.PNG");
+            break;
+        case 2:
+            m_pSharedImage = Fl_Shared_Image::get("Wizard2.PNG");
+            break;
+        case 3:
+            m_pSharedImage = Fl_Shared_Image::get("Wizard3.PNG");
+            break;
+        case 4:
+            m_pSharedImage = Fl_Shared_Image::get("Wizard4.PNG");
+            break;
+        }
+        image(m_pSharedImage->copy(w(),h()));
+        redraw();
+    }
+private:
+    Fl_Shared_Image* m_pSharedImage;
+ };
+
+#endif // DOTBOX_H_INCLUDED
+
