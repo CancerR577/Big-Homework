17becaa0d8b26f3672d80ed3917d340623073aa6
Tiffany He <1700012189@pku.edu.cn>
2019-01-10 14:49:56 +0800

    初回限定版_game_final_version
    加上了背景，游戏大致就是这样了

---
+++ OurGame/Ver5.3/SimpleBoard.cpp
@@ -0,0 +1,186 @@
+#include "SimpleBoard.h"
+#include <iostream>
+#include <algorithm>
+
+using std::min;
+
+SimpleBoard::SimpleBoard(int r, int c,StartAndEnd* sae, int t)
+    : Squares::Squares(r,c,sae,t)
+    {
+        if (t == 1)
+        {
+            const int X { 50 };
+            const int Y { 100 };
+            const int LX { 900 };
+            const int LY { 600 };
+            const int L { min( LY / (r + 1), LX / (c + 1)) };
+            const int D { L / 10 };
+            const int x { X  + (LX - c * L - D) / 2 };
+            const int y { Y  + (LY - r * L - D) / 2 };
+
+            int i, j;
+            EnterBox *HeBox[r + 1][c];//Horizontal EnterBox
+            EnterBox *VeBox[r][c + 1];//Vertical EnterBox
+            DegTable = new int *[r];
+            for(i = 0; i < r; i++ )
+            {
+                DegTable[i] = new int [c];
+            }
+
+            for (i = 0; i < r + 1; i++)
+            {
+                for (j = 0; j < c; j++)
+                {
+                    if (i == 0)
+                    {
+                        HeBox[i][j] = new EnterBox( x + D + L * j, y + L * i, L - D , D , s[0][j]);
+                        HeBox[i][j]->box(FL_PLASTIC_DOWN_FRAME);
+                    }
+                    else if (i == r)
+                    {
+                        HeBox[i][j] = new EnterBox( x + D + L * j, y + L * i, L - D , D , s[r - 1][j]);
+                        HeBox[i][j]->box(FL_PLASTIC_DOWN_FRAME);
+                    }
+                    else
+                    {
+                        HeBox[i][j] = new EnterBox( x + D + L * j, y + L * i, L - D , D , s[i - 1][j], s[i][j]);
+                        HeBox[i][j]->box(FL_PLASTIC_DOWN_FRAME);
+                    }
+                }
+            }
+
+
+            for (i = 0; i < r; i++)
+            {
+                for (j = 0; j < c + 1; j++)
+                {
+                    if (j == 0)
+                    {
+                        VeBox[i][j] = new EnterBox( x + L * j, y + D + L * i, D , L - D , s[i][0]);
+                        VeBox[i][j]->box(FL_PLASTIC_DOWN_FRAME);
+                    }
+                    else if (j == c)
+                    {
+                        VeBox[i][j] = new EnterBox( x + L * j, y + D + L * i, D , L - D , s[i][c - 1]);
+                        VeBox[i][j]->box(FL_PLASTIC_DOWN_FRAME);
+                    }
+                    else
+                    {
+                        VeBox[i][j] = new EnterBox( x + L * j, y + D + L * i, D , L - D , s[i][j - 1], s[i][j]);
+                        VeBox[i][j]->box(FL_PLASTIC_DOWN_FRAME);
+                    }
+                }
+            }
+
+            for(i = 0; i < r; i++){
+                for(j = 0; j < c; j++)
+                {
+                    DegTable[i][j] = s[i][j]->getdeg();
+                }
+            }
+        }
+        if (t == 2)
+        {
+            const int X { 50 };
+            const int Y { 100 };
+            const int LX { 900 };
+            const int LY { 600 };
+            const int L { min(  LY / (r + (c + 1) / 2), LX / (c + 1)) };
+            const int D { L / 10 };
+            const int x { X  + (LX - c * L - D) / 2 };
+            const int y { Y  + (LY - r * L - (c - 1) * L / 2 - D) / 2 };
+            int i, j;
+            EnterBox *HeBox[r + 1][c];//Horizontal EnterBox
+            EnterBox *VeBox1[r][c + 1];//Vertical EnterBox
+            EnterBox *VeBox2[r + 1][c + 1];//Vertical EnterBox
+            DegTable = new int *[r];
+            for(i = 0; i < r; i++ )
+            {
+                DegTable[i] = new int [c];
+            }
+
+            for (i = 0; i < r + 1; i++)
+            {
+                for (j = 0; j < c; j++)
+                {
+                    if (i == 0)
+                    {
+                        HeBox[i][j] = new EnterBox(x + D + L * j, y + L * i + L * j / 2, L - D, D, s[0][j]);
+                        HeBox[i][j]->box(FL_PLASTIC_DOWN_FRAME);
+                    }
+                    else if (i == r)
+                    {
+                        HeBox[i][j] = new EnterBox(x + D + L * j, y + L * i + L * j / 2, L - D, D, s[r - 1][j]);
+                        HeBox[i][j]->box(FL_PLASTIC_DOWN_FRAME);
+                    }
+                    else
+                    {
+                        HeBox[i][j] = new EnterBox(x + D + L * j, y + L * i + L * j / 2, L - D, D, s[i - 1][j], s[i][j]);
+                        HeBox[i][j]->box(FL_PLASTIC_DOWN_FRAME);
+                    }
+                }
+            }
+
+
+            for (i = 0; i < r; i++)
+            {
+                for (j = 0; j < c + 1; j++)
+                {
+                    if (j == 0)
+                    {
+                        VeBox1[i][j] = new EnterBox(x + L * j, y + D + L * i + L * j / 2, D, L / 2 - D, s[i][0]);
+                        VeBox1[i][j]->box(FL_PLASTIC_DOWN_FRAME);
+                    }
+                    else if (j == c)
+                    {
+                        VeBox1[i][j] = new EnterBox(x + L * j, y + D + L * i + L * j / 2, D, L / 2 - D, s[i][c - 1]);
+                        VeBox1[i][j]->box(FL_PLASTIC_DOWN_FRAME);
+                    }
+                    else
+                    {
+                        VeBox1[i][j] = new EnterBox(x + L * j, y + D + L * i + L * j / 2, D, L / 2 - D, s[i][j - 1], s[i][j]);
+                        VeBox1[i][j]->box(FL_PLASTIC_DOWN_FRAME);
+                    }
+                }
+            }
+
+            for (i = 0; i < r + 1; i++)
+            {
+                for (j = 0; j < c + 1; j++)
+                {
+                    if (i == 0 && j != 0)
+                    {
+                        VeBox2[i][j] = new EnterBox(x + L * j, y - L / 2 + D + L * i + L * j / 2, D, L / 2 - D, s[0][j - 1]);
+                        VeBox2[i][j]->box(FL_PLASTIC_DOWN_FRAME);
+                    }
+                    else if (i == r && j != c)
+                    {
+                        VeBox2[i][j] = new EnterBox(x + L * j, y - L / 2 + D + L * i + L * j / 2, D, L / 2 - D, s[r - 1][j]);
+                        VeBox2[i][j]->box(FL_PLASTIC_DOWN_FRAME);
+                    }
+                    else if (i != 0 && j == 0)
+                    {
+                        VeBox2[i][j] = new EnterBox(x + L * j, y - L / 2 + D + L * i + L * j / 2, D, L / 2 - D, s[i - 1][0]);
+                        VeBox2[i][j]->box(FL_PLASTIC_DOWN_FRAME);
+                    }
+                    else if (i != r && j == c)
+                    {
+                        VeBox2[i][j] = new EnterBox(x + L * j, y - L / 2 + D + L * i + L * j / 2, D, L / 2 - D, s[i][c - 1]);
+                        VeBox2[i][j]->box(FL_PLASTIC_DOWN_FRAME);
+                    }
+                    else if (i != 0 && i != r && j != 0 && j != c)
+                    {
+                        VeBox2[i][j] = new EnterBox(x + L * j, y - L / 2 + D + L * i + L * j / 2, D, L / 2 - D, s[i][j - 1], s[i - 1][j]);
+                        VeBox2[i][j]->box(FL_PLASTIC_DOWN_FRAME);
+                    }
+                }
+            }
+
+            for(i = 0; i < r; i++){
+                for(j = 0; j < c; j++)
+                {
+                    DegTable[i][j] = s[i][j]->getdeg();
+                }
+            }
+        }
+    }
---
+++ OurGame/Ver5.3/SquareUnit.h
@@ -0,0 +1,34 @@
+#ifndef _SQUAREUNIT_H_
+#define _SQUAREUNIT_H_
+
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Widget.H>
+#include <FL/Fl_Shared_Image.H>
+
+class Squares;
+
+class SquareUnit : public Fl_Box
+{
+public:
+    SquareUnit(int x, int y, int w, int h, int d, Squares* sqss);
+    int getdeg();
+    void setdeg(int d);
+    void changePlayer();
+    void activation();
+    void activation2();
+    void callSquares();
+
+private:
+    int deg;
+    int PlayerNumber;
+    Squares* sqs;
+    Fl_Shared_Image *square_pic;
+};
+
+
+#endif // SQUAREUNIT
+
+
